6bd2c6c3303daec3128a2ce84e348d7dd862ab84
Cory Silva <coryasilva@gmail.com>
2015-09-14 09:42:49 -0700

    Overhaul AngularJS client

--- client/bugLogClientAngular.js
+++ client/bugLogClientAngular.js
@@ -1,11 +1,16 @@
+/**
+ * This module requires stacktrace-js and angular, it has been tested with:
+ *
+ * "stacktrace-js": "~0.6.4"
+ * "angular": "~1.3.15"
+ */
+
 angular.module('BugLogHq', [])
     //the application to tell BugLog from where the report is coming from.
     .constant('appName', "app")
     .constant('bugLogConfig', {
         // this is the location of the BugLog server that will be receiving the error reports (needs to be a REST/POST endpoint)
         listener: "",
-        // the hostname to tell BugLog from where the report is coming from. Leave empty to get the info from the browser.
-        hostName: "",
         // If the BugLog server requires an API Key to talk to it, then here is where you put it.
         apiKey: ""
     })
@@ -16,10 +21,7 @@ angular.module('BugLogHq', [])
                     return function(exception, cause) {
                         //Custom error handling.
                         try {
-                            BugLogService.notifyService({
-                                message: cause,
-                                error: exception
-                            });
+                            BugLogService.logBug(cause, exception);
                         }
                         catch (e) {
                             $log.warn("BugLog Service: Error logging bug");
@@ -32,114 +34,81 @@ angular.module('BugLogHq', [])
             ]);
         }
     ])
-    .factory('BugLogService', function BugLogService($window, appName, bugLogConfig) {
-        var service = {
+    .factory('BugLogService', function BugLogService($window, $log, appName, bugLogConfig) {
+        var config = {
             // this is the location of the BugLog server that will be receiving the error reports (needs to be a REST/POST endpoint)
             listener: bugLogConfig.listener,
-
-            // the hostname to tell BugLog from where the report is coming from. Leave empty to get the info from the browser.
-            hostName: bugLogConfig.hostName,
-
             //the application to tell BugLog from where the report is coming from.
             appName: appName,
-
             // If the BugLog server requires an API Key to talk to it, then here is where you put it.
-            apiKey: bugLogConfig.apiKey,
-            notifyService: notifyService,
-            createNotifier: createNotifier,
-            destroyNotifier: destroyNotifier,
-            getStylesheets: getStylesheets,
-            getScripts: getScripts,
-            getRootScope: getRootScope,
-            cleanScope: cleanScope
+            apiKey: bugLogConfig.apiKey
+        };
+
+        var service = {
+            logBug: logBug
         };
 
         return service;
         /////////////////////
+
         /**
          * Communicates with a BugLogHq Server
-         * @param message
+         * @param {string} cause
+         * @param {object} error
+         * @param {string} severityCode
          */
-        function notifyService(message) {
-            var msg = {};
-            var htmlReport = '<h3>Bug Report</h3>';
+        function logBug(cause, error, severityCode) {
+            var data = {};
+            data.message = 'Caused by: ' + cause;
+            data.exceptionMessage = cause || 'Unknown';
+            data.exceptionDetails =  buildStacktrace(error) || 'something went wrong';
+            data.severityCode = severityCode || 'EXCEPTION';
+            data.HTMLReport = buildHtmlReport() || 'something went wrong';
+            data.userAgent = navigator.userAgent || 'Unknown';
+            data.templatePath = document.URL || 'Unknown';
+            data.applicationCode = config.appName || 'Unknown';
+            data.APIKey = config.apiKey || '';
 
-            // Message Defaults
-            if(typeof message === 'string') {
-                msg.message = message;
+            // Make server call
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', encodeURI(config.listener));
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.send(JSON.stringify(data));
+
+        }
+
+        /**
+         * Build Stacktrace
+         * @param error
+         * @return {*}
+         */
+        function buildStacktrace(error) {
+            var stack;
+            if(typeof error.stack !== 'undefined') {
+                stack = error.stack;
             }
-            else {
-                msg.message = message.message;
+            if ($window.printStackTrace) {
+                stack = $window.printStackTrace({e: error}).join('\n');
             }
-            msg.error = message.error || undefined;
-            msg.severity = message.severity || 'ERROR';
-            msg.stack = message.stack || undefined;
-            msg.errorUrl = message.errorUrl || $window.location.href;
-            msg.stylesheets = message.stylesheets || service.getStylesheets();
-            msg.scripts = message.scripts || service.getScripts();
-            msg.rootScope = message.rootScope || service.getRootScope();
+            return stack;
+        }
+
+        /**
+         * Build HTML Report
+         * @returns {string}
+         */
+        function buildHtmlReport() {
+            var html = '<h3>Bug Report</h3>';
+            var stylesheets = getStylesheets() || 'something went wrong';
+            var scripts = getScripts() || 'something went wrong';
+            var rootScope = getRootScope() || 'something went wrong';
 
             // URL
-            htmlReport += '<h4>Error Url:</h4><pre>' + msg.errorUrl + '</pre>';
+            html += '<h4>RootScope:</h4><pre>' + angular.toJson(rootScope, 2) + '</pre>';
+            html += '<h4>Scripts:</h4><pre>' + angular.toJson(scripts, 2) + '</pre>';
+            html += '<h4>Stylesheets:</h4><pre>' + angular.toJson(stylesheets, 2) + '</pre>';
 
-            // Stacktrace
-            var stacktrace;
-            if(typeof msg.stack !== 'undefined') {
-                stacktrace = msg.stack;
-            }
-            else if(typeof msg.error !== 'undefined') {
-                stacktrace = (typeof msg.error.stack !== 'undefined') ? msg.error.stack : $window.printStackTrace({e: msg.error}).join('\n');
-            }
-            if(typeof stacktrace !== 'undefined') {
-                htmlReport += '<h4>Stacktrace:</h4><pre>' + stacktrace + '</pre>';
-            }
-
-            // Additional Information (if any)
-            if($window.JSON) {
-                htmlReport += '<h4>Stylesheets:</h4><pre>' + $window.JSON.stringify(msg.stylesheets) + '</pre>';
-                htmlReport += '<h4>Scripts:</h4><pre>' + $window.JSON.stringify(msg.scripts) + '</pre>';
-                htmlReport += '<h4>RootScope:</h4><pre>' + $window.JSON.stringify(msg.rootScope) + '</pre>';
-            }
-
-            // Build the message as URL parameter
-            var data = 'message=' + escape(msg.message) +
-                '&severityCode=' + escape(msg.severity) +
-                '&hostName=' + escape(service.hostName || $window.location.host) +
-                '&applicationCode=' + escape(service.appName) +
-                '&apiKey=' + escape(service.apiKey) +
-                '&userAgent=' + escape(navigator.userAgent) +
-                '&templatePath=' + escape(document.URL) +
-                '&htmlReport=' + escape(htmlReport);
-
-            // create the notifier
-            var noti = service.createNotifier('script');
-            noti.src = service.listener + '?' + data;
-
-            // destroy the notifier
-            service.destroyNotifier(noti);
-        }
-
-        /**
-         * Creates an Html element in an Html Document's <head>
-         * @param tagName
-         * @returns {HTMLElement}
-         */
-        function createNotifier(tagName) {
-            var notifier = document.createElement(tagName);
-            notifier.id = 'buglog' + (+ new Date());
-            var head = document.getElementsByTagName('head')[0];
-            head.appendChild(notifier);
-            return notifier;
-        }
-
-        /**
-         * Destroys an Html element created by createNotifier()
-         * @param notifier
-         */
-        function destroyNotifier(notifier) {
-            var elm = document.getElementById(notifier.id);
-            var head = document.getElementsByTagName('head')[0];
-            head.removeChild(elm);
+            return html;
         }
 
         /**
@@ -151,7 +120,8 @@ angular.module('BugLogHq', [])
             var links = document.getElementsByTagName('link');
             for(var i = 0; i < links.length; i++) {
                 if (links[i].href && links[i].rel && links[i].rel.toLowerCase() === 'stylesheet') {
-                    assets.push(links[i].href);
+                    var href = links[i].href.split('/');
+                    assets.push(href[href.length-1]);
                 }
             }
             return assets;
@@ -166,7 +136,8 @@ angular.module('BugLogHq', [])
             var scripts = document.getElementsByTagName('script');
             for(var j = 0; j < scripts.length; j++) {
                 if (scripts[j].src) {
-                    assets.push(scripts[j].src);
+                    var src = scripts[j].src.split('/');
+                    assets.push(src[src.length-1]);
                 }
             }
             return assets;
